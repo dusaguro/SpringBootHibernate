845cac37e665f63643f39c245da1200386a4f259
DUSAGURO <dusaguro@outlook.com>
2026-08-19 10:35:04 -0500

    Implementacion de CommandLineRunner, Creacion de entidad Person y mapeo de Person

---
+++ src/main/java/com/dsgr/springboot/jpa/springdatajpa/SpringdatajpaApplication.java
@@ -0,0 +1,19 @@
+package com.dsgr.springboot.jpa.springdatajpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
