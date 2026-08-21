09e7b2ee7e44c7f0a6462f5b7362941adef3768f
DUSAGURO <dusaguro@outlook.com>
2026-08-20 22:56:24 -0500

    Metodos para crear y actualizar hechos

--- src/main/java/com/dsgr/springboot/jpa/springdatajpa/SpringdatajpaApplication.java
+++ src/main/java/com/dsgr/springboot/jpa/springdatajpa/SpringdatajpaApplication.java
@@ -2,11 +2,13 @@ package com.dsgr.springboot.jpa.springdatajpa;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dsgr.springboot.jpa.springdatajpa.entities.Person;
 import com.dsgr.springboot.jpa.springdatajpa.repositories.PersonRepository;
@@ -25,15 +27,59 @@ public class SpringdatajpaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		// list();
-		findOne();
+		// findOne();
+		// create();
+		update();
+	}
+
+	@Transactional
+	public void update() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Ingrese el id a actualizar:");
+		Long id = scanner.nextLong();
+
+		Optional<Person> person = personRepository.findById(id);
+
+		person.ifPresentOrElse(p -> {
+			System.out.println(p);
+			System.out.println("Ingrese el nuevo lenguaje:");
+			String programmingLanguage = scanner.next();
+			p.setProgrammingLanguage(programmingLanguage);
+			Person personDB = personRepository.save(p);
+			System.out.println(personDB);
+		}, () -> System.out.println("No existe registro con el id: " + id));
+
+		scanner.close();
 
 	}
 
+	@Transactional
+	public void create() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Name:");
+		String name = sc.next();
+
+		System.out.println("LastName:");
+		String lastname = sc.next();
+
+		System.out.println("ProgrammingLanguage:");
+		String programmingLanguage = sc.next();
+
+		sc.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage);
+		Person personNew = personRepository.save(person);
+		personRepository.findById(personNew.getId()).ifPresent(System.out::println);
+	}
+
+	@Transactional(readOnly = true)
 	public void findOne() {
 		// Person person = null;
 		// Optional<Person> optionalPerson = personRepository.findById(8L);
 		// if(optionalPerson.isPresent()){
-		// 	person=optionalPerson.get();
+		// person=optionalPerson.get();
 		// }
 		// System.out.println(person);
 
@@ -42,9 +88,9 @@ public class SpringdatajpaApplication implements CommandLineRunner {
 		// personRepository.findOneName("Pepe").ifPresent(System.out::println);
 		personRepository.findOneLikeName("se").ifPresent(System.out::println);
 
-
 	}
 
+	@Transactional(readOnly = true)
 	public void list() {
 		// List<Person> persons = (List<Person>) personRepository.findAll();
 		// List<Person> persons = (List<Person>)
